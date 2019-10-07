ca0d249fcfb87beffcd3610d802d05289892eca8
Markus Felten <Markus.Felten@gmx.de>
2019-10-07 21:42:18 +0200

    feat: another type ov workaround with rollup-plugin-virtual

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,19 @@
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
+import virtual from "rollup-plugin-virtual";
 
 const external = ["universal-user-agent"];
 const plugins = [resolve(), commonjs()];
 
-
 export default [
+  {
+    output: {
+      file: "dist/bundeled-with-rollup-cjs-not-working",
+      format: "cjs"
+    },
+    plugins,
+    input: "index.mjs"
+  },
   {
     output: {
       file: "dist/bundeled-with-rollup-cjs-external-workaround",
@@ -17,10 +25,16 @@ export default [
   },
   {
     output: {
-      file: "dist/bundeled-with-rollup-cjs-not-working",
+      file: "dist/bundeled-with-rollup-cjs-patched",
       format: "cjs"
     },
-    plugins,
+    plugins: [
+      virtual({
+        "universal-user-agent":
+          "export function getUserAgent() {return `Node.js/${process.version.substr(1)} (${process.platform}); ${process.arch})`}"
+      }),
+      ...plugins
+    ],
     input: "index.mjs"
   }
 ];
